220db08f58ea4dcf523e78ab03c8b210d85c364d
Gustavo Almeida <gspalmeida@icloud.com>
2023-05-04 01:14:44 -0300

    feat: pages structure

---
+++ app/contato/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contato() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-900">
+      <h1>Contato</h1>
+    </main>
+  )
+}
