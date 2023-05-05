901bc30b079bc1549f8637e3176d246e4123f16d
Gustavo Almeida <gspalmeida@icloud.com>
2023-05-05 02:58:06 -0300

    feat: Create structure of the Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-700 w-full h-[50vh]">
+      <h1 className="text-xl text-center w-full">Footer</h1>
+    </footer>
+  )
+}
