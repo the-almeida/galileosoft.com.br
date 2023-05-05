d3b303194adffae759aa81ee3bf8e8085be25056
Gustavo Almeida <gspalmeida@icloud.com>
2023-05-05 02:54:28 -0300

    feat: Create component BackgroundImage

---
+++ components/BackgroundImage.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+interface ImageProps {
+    src?: string;
+    withOverlay?: boolean;
+}
+
+const BackgroundImage = ({ src = "/defaultBgImage.jpg", withOverlay = false }: ImageProps) => {
+    return (
+        <>
+            <div className="absolute -z-20 w-full h-screen">
+                {withOverlay && <div className="bg-gray-900/50 md:bg-gray-900/20 w-full h-screen z-0" /> }
+                <Image
+                    className="-z-10"
+                    src={src}
+                    alt="Background Image purple with a rocket."
+                    fill
+                />
+            </div>
+        </>
+    );
+};
+
+export default BackgroundImage;
