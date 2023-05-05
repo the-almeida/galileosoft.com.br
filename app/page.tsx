a0616854ce9938e8bbd2a0d92ba61926a8323bb5
Gustavo Almeida <gspalmeida@icloud.com>
2023-05-05 02:59:08 -0300

    refactor: Home page heroSection, responsive and layout improvments

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,64 @@
-import Image from "next/image";
+import BackgroundImage from "@/components/BackgroundImage";
+import Container from "@/components/FlexContainer";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-      <div className="fixed -z-20 w-full h-screen">
-        <div className="bg-gray-900/50 md:bg-gray-900/20 w-full h-screen z-0" />
-        {/* TODO Improve this asset and structure: only bg here with overlay + the rocket inside the layout below (review unnecessary margins and width like 'sm:w-3/4 lg:w-1/2 xl:pr-[5%]') */}
-        <Image
-          className="-z-10"
-          src="/bgImageHomePageHeroSection.jpg"
-          alt="Background Image purple with a rocket."
-          fill
-        />
-      </div>
-      <main className="flex h-screen max-w-7xl mx-auto flex-col items-center justify-between p-6 md:p-10 lg:p-16">
-        <section className="h-full" id="hero-section">
-          <div className="flex flex-col justify-center gap-5 text-white h-full text-center sm:text-left sm:w-3/4 lg:w-1/2 xl:pr-[5%]">
-            <h1 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4">
-              Inove e cresça com tecnologia de ponta.
-            </h1>
-            <p className="text-base md:text-lg lg:text-xl xl:text-2xl">
-              Atuamos como agentes integradores entre as melhores ferramentas digitais do mercado, desenvolvedores altamente capacitados e a realidade tecnológica da sua empresa.
+      {/* TODO Improve the bgImageHomePageHeroSection, move the rocket to the #heroSection*/}
+      
+      <main className="flex flex-col items-center justify-between">
+        <section className="h-screen relative w-full" id="heroSection">
+          <BackgroundImage
+            src="/bgImageHomePageHeroSection.jpg"
+            withOverlay
+          />
+          <Container className="flex-row">
+            <div className="flex flex-col justify-center gap-5 text-white md:w-2/3 px-3 md:px-6 lg:px-8">
+              <h1 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4">
+                Inove e cresça com tecnologia de ponta.
+              </h1>
+              <p className="text-base md:text-lg lg:text-xl xl:text-2xl">
+                Atuamos como agentes integradores entre as melhores ferramentas digitais do mercado, desenvolvedores altamente capacitados e a realidade tecnológica da sua empresa.
+              </p>
+              <p className="text-base md:text-lg lg:text-xl xl:text-2xl">
+                Somos seus parceiros em tecnologia para crescer e inovar com tecnologia de ponta.
+              </p>
+            </div>
+            <div className="absolute md:relative min-h-screen md:min-h-fit w-full flex flex-1 justify-center bg-slate-500/50 mt-16 -z-10">
+              <p>Foguete</p>
+            </div>
+          </Container>
+        </section>
+
+        <section className="bg-red-600 h-screen" id="aboutSection">
+          <h2 className="text-2xl">Conheça a GalileoSoft</h2>
+          <div>
+            <h3 className="text-2xl">São mais de 7 anos de história</h3>
+            <p>
+              A GalileoSoft surgiu em 2016 com foco em posicionamento digital associado a softwares personalizados.
             </p>
-            <p className="text-base md:text-lg lg:text-xl xl:text-2xl">
-              Somos seus parceiros em tecnologia para crescer e inovar com tecnologia de ponta.
+          </div>
+          <div>
+            <h3>A inovação é nossa essência</h3>
+            <p>
+              Desde a nossa entrada no mercado buscamos evoluir constantemente. Por conta disso notamos que muitas empresas que nos contratavam não tinham acesso às mais novas e modernas ferramentas de gestão.
+              <strong>Estamos aqui para acabar com esse problema.</strong>
+            </p>
+
+          </div>
+          <div>
+            <h3>O nosso diferencial</h3>
+            <p>
+              Identificamos que integrar e adaptar ferramentas modernas de altíssima qualidade (com reconhecimento nacional) aos negócios tradicionais seria o caminho que trilharíamos.
+              <strong> 
+                Nosso trunfo é conseguir conectar a realidade da operação às ferramentas de gestão com agilidade.
+              </strong>
             </p>
           </div>
         </section>
       </main>
+      <Footer />
     </>
   )
 }
