b6170a7995d97c730cb2174d9c530d4ec70c32c3
Gustavo Almeida <gspalmeida@icloud.com>
2023-05-08 15:22:12 -0300

    feat: Home page aboutSection v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import BackgroundImage from "@/components/BackgroundImage";
 import Card from "@/components/Card";
 import ContentContainer from "@/components/ContentContainer";
-import { MdPhone } from "react-icons/md";
+import Particles from "@/components/Particles";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -23,46 +24,58 @@ export default function Home() {
               <p className="text-base md:text-lg lg:text-xl xl:text-2xl">
                 Somos seus parceiros em tecnologia para crescer e inovar com tecnologia de ponta.
               </p>
-              <button className="flex justify-center gap-4 rounded-sm bg-gray-600 w-fit px-6 py-4">
-                <MdPhone size={18} /> TODO: Definir a CTA
-              </button>
+              <Link href="https://calendly/criar-evento-newton-account" className="bg-gradient-to-br from-pink-600 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-900 text-white text-lg font-medium rounded-lg w-fit px-5 py-2.5 text-center mr-2 mb-2">
+                Agendar reunião
+              </Link>
             </div>
-            <div className="fixed md:relative flex flex-1 justify-center bg-slate-500/50 -ml-3 md:-ml-8 lg:-ml-13 -z-10">
-              <p>TODO: Trocar o backckgroud para um sem foguete e adicionar só o foguete no lugar desse paragrafo</p>
+            <div className="fixed md:relative flex flex-1 justify-center -ml-3 md:-ml-8 lg:-ml-13 -z-10">
+              {/* <p>TODO: Trocar o backckgroud para um sem foguete e adicionar só o foguete no lugar desse paragrafo</p> */}
             </div>
           </ContentContainer>
         </section>
 
-        <section className="h-screen relative w-full" id="aboutSection">
-          <BackgroundImage src="/bgImageHomePageAboutSection.jpg" withOverlay opacityScale={0.25} />
-          <ContentContainer className="flex flex-col space-y-4 py-8">
-            <h2 className="text-4xl text-center">Conheça a GalileoSoft</h2>
-            <div className="block lg:flex gap-6">
+        <section className="w-full flex bg-black relative min-h-[65vh]" id="aboutSection">
+          <Particles
+            className="absolute animate-fade-in"
+            quantity={200}
+          />
+          <ContentContainer className="flex flex-col text-black my-auto z-10">
+            <span className="text-orange-500 font-bold text-center">
+              São mais de 7 anos de história!
+            </span>
+            <h2 className="text-3xl text-center text-white font-bold">
+              Conheça a GalileoSoft
+            </h2>
+            <div className="block lg:flex gap-6 mt-10">
               <Card>
-                <h3 className="text-2xl font-bold text-black text-center mb-2">São mais de 7 anos de história</h3>
+                <h3 className="text-2xl font-bold text-black text-center mb-2">
+                  Gestão inteligente
+                </h3>
                 <p className="text-gray-600 leading-normal">
-                  A GalileoSoft surgiu em 2016 com foco em posicionamento digital associado a softwares personalizados. Desde então, utilizamos nossa expertise em gestão e tecnologia para oferecer soluções personalizadas e inovadoras para atender às necessidades específicas de cada cliente.
+                  Utilizamos nossa expertise de gestão em conjunto com nosso conhecimento tecnológico para identificar e resolver gargalos nos processos operacionais da sua empresa com agilidade.
                 </p>
               </Card>
-
               <Card>
-                <h3 className="text-2xl font-bold text-black text-center mb-2">A inovação é nossa essência</h3>
+                <h3 className="text-2xl font-bold text-black text-center mb-2">Inovação constante</h3>
                 <p className="text-gray-600 leading-normal">
-                  Desde nossa entrada no mercado, evoluímos constantemente para oferecer soluções modernas e eficazes. Muitas empresas não têm acesso às ferramentas de gestão avançadas, comprometendo sua competitividade. Estamos aqui para oferecer soluções personalizadas e avançadas que permitem que nossos clientes se destaquem.
+                  Trabalhamos incansavelmente avaliando as tendências do mercado de tecnologia para conseguirmos oferecer soluções personalizadas, inovadoras e que atenderão suas necessidades por um longo prazo.
                 </p>
               </Card>
-
               <Card>
-                <h3 className="text-2xl font-bold text-black text-center mb-2">O nosso diferencial</h3>
+                <h3 className="text-2xl font-bold text-black text-center mb-2">Mais do que Software</h3>
                 <p className="text-gray-600 leading-normal">
-                  Comprovamos em nossa experiência que a integração de ferramentas modernas de alta qualidade aos diferentes modelos de negócio é fundamental para o sucesso das empresas. O nosso diferencial está em conseguir conectar a realidade da operação às ferramentas de gestão com agilidade e eficiência, oferecendo uma solução personalizada e eficaz para cada cliente.
+                  O nosso diferencial é entender o lado humano e operacional de cada empresa para criar uma experiência encantadora para os seus clientes, prática para os seus colaboradores e lucrativa para você.
                 </p>
               </Card>
             </div>
-
-            <button className="flex justify-center gap-4 rounded-sm bg-gray-600 w-fit mx-auto px-6 py-4">
-              <MdPhone size={18} /> TODO: Definir a CTA
-            </button>
+            <div className="flex flex-col gap-5 mt-10">
+              <p className="text-lg leading-5 text-center font-semibold w-1/2 mx-auto">
+                Estamos prontos expandir os horizontes do seu negócio, vamos juntos nessa jornada?
+              </p>
+              <Link href="https://calendly/criar-evento-newton-account" className="bg-gradient-to-br from-pink-600 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-900 text-white text-lg font-medium rounded-lg w-fit px-5 py-2.5 text-center mx-auto">
+                Falar com especialista!
+              </Link>
+            </div>
           </ContentContainer>
         </section>
       </main>
