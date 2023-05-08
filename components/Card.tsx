94a340a77e727986732218265208b8828824a3fc
Gustavo Almeida <gspalmeida@icloud.com>
2023-05-08 15:21:01 -0300

    refactor: Card component children aligment set to top

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,7 @@ export default function Card({ children }: ContainerProps) {
   return (
                 
     <div x-componentname="Card" className="flex flex-1 bg-slate-200 text-center rounded-lg overflow-hidden shadow-lg p-6 md:p-7 lg:p-10">
-      <div className="my-auto">
+      <div className="mb-auto">
         {children}
       </div>
     </div>
