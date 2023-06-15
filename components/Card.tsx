c40e291d8384d2033e226337a1aa84b8162ef2ff
Gustavo Almeida <gspalmeida@icloud.com>
2023-06-15 10:57:51 -0300

    refactor: Card readequado à identidade visual

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,10 +4,9 @@ interface CardProps {
 }
 
 export default function Card({ children, className = '' }: CardProps) {
-  return (
-                
-    <div x-componentname="Card" className="flex flex-1 bg-white text-center rounded-lg overflow-hidden shadow-lg p-6 md:p-7 lg:p-10">
-      <div className={`${className} mb-auto w-full` }>
+  return ( 
+    <div x-componentname="Card" className="relative flex flex-1 bg-galileoGray-400 text-center rounded-lg overflow-hidden shadow-lg p-6 md:p-7 lg:p-10">
+      <div className={`mb-auto w-full ${className}`}>
         {children}
       </div>
     </div>
