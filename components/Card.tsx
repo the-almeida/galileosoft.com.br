036e9fd399fea72cc17fc39706e3573fd4a8f317
Gustavo Almeida <gspalmeida@icloud.com>
2023-08-08 12:33:34 -0400

    fix: card bg-color

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 
 export default function Card({ children, className = '' }: CardProps) {
   return ( 
-    <div x-componentname="Card" className="relative flex flex-1 bg-galileoGray-400 text-center rounded-lg overflow-hidden shadow-lg p-6 md:p-7 lg:p-10">
+    <div x-componentname="Card" className="relative flex flex-1 bg-slate-100 text-center rounded-lg overflow-hidden shadow-lg p-6 md:p-7 lg:p-10">
       <div className={`mb-auto w-full ${className}`}>
         {children}
       </div>
