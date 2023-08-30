fc9b3c477971f4bdf367d18b0da3c3a88eb62694
Gustavo Almeida <gustavo.s.p.almeida@gmail.com>
2023-08-29 20:31:23 -0400

    refactor: remove unecessary page and assets

--- app/produtos/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Produtos() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-900">
-        <h1>Produtos</h1>
-      </main>
-    )
-  }
-  
