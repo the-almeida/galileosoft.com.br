d2db0b78db639913ebbdcb1c34581ed4aec9cb3b
Gustavo Almeida <gustavo.s.p.almeida@gmail.com>
2023-09-13 20:27:02 -0400

    fix: remoção de imports desnecessários

--- app/ploomes/page.tsx
+++ app/ploomes/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import ButtonOrange from "@/components/ButtonOrange";
 import ButtonGreen from "@/components/ButtonGreen";
 import Card from "@/components/Card";
 import Carousel from "@/components/Carousel";
