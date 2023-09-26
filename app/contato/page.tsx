53bd092b6ae092c95069d0826b9a3d6f0511e751
Gustavo Almeida <gustavo.s.p.almeida@gmail.com>
2023-09-25 22:37:19 -0300

    feat: contact page responsiveness

--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -5,30 +5,34 @@ import ContactForm from "@/components/ContactForm"
 import ContentContainer from "@/components/ContentContainer"
 import { MdFacebook, MdPhone, MdWhatsapp } from "react-icons/md"
 import { FaInstagram } from "react-icons/fa"
+import PrimaryHeading from "@/components/PrimaryHeading"
+import SecundaryText from "@/components/SecundaryText"
+import SecundaryHeading from "@/components/SecundaryHeading"
+import PrimaryText from "@/components/PrimaryText"
 
 export default function Contato() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-16 bg-purple-950">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-16 bg-purple-950 text-white">
       <section className="relative w-full">
         <BackgroundImage src="/bgImageWaves.svg" className="-mt-20 opacity-75" />
-        <ContentContainer className="w-full grid grid-cols-2 gap-x-20 gap-y-2 justify-between items-start relative z-10 my-10">
+        <ContentContainer className="w-full grid grid-cols-1 lg:grid-cols-2 gap-x-20 gap-y-2 justify-between items-start relative z-10 my-10">
           <div>
-            <span className="text-xl md:text-2xl font-semibold text-green-500">ENTRE EM CONTATO</span>
-            <h1 className="text-2xl md:text-4xl lg:text-5xl xl:text-6xl mb-4 font-semibold pt-3 pb-8">Pronto para modernizar sua empresa?</h1>
-            <p className="text-xl md:text-2xl text-gray-200 font-medium">
+            <span className="hidden lg:block text-xl md:text-2xl font-semibold text-green-500">ENTRE EM CONTATO</span>
+            <PrimaryHeading className="text-center lg:text-left">Pronto para modernizar sua empresa?</PrimaryHeading>
+            <SecundaryText className="text-center lg:text-left">
               Fornecemos diversos produtos que são verdadeiros campeões seus segmentos com a garantia de customização completa para atender seu negócio.
-            </p>
+            </SecundaryText>
           </div>
           <Card>
-            <h1 className="text-purple-950 text-3xl text-left font-semibold mb-4">Seja um case de sucesso</h1>
+            <SecundaryHeading className="text-purple-950 text-center lg:text-left">Seja um case de sucesso</SecundaryHeading>
             <ContactForm />
           </Card>
-          <div className="col-start-2">
-            <p className="text-center text-md font-semibold opacity-80 px-5">Ao enviar esse formulário, você concorda em receber contatos da GalileoSoft.</p>
+          <div className="lg:col-start-2">
+            <SecundaryText className="text-center text-md font-semibold opacity-80 px-5">Ao enviar esse formulário, você concorda em receber contatos da GalileoSoft.</SecundaryText>
           </div>
-          <div className="text-black flex-1 space-y-3 col-span-2 py-3">
-            <h2 className="text-white text-center text-4xl font-medium mb-6">Outros canais de contato:</h2>
-            <div className="w-fit mx-auto grid gap-4 grid-cols-2 lg:grid-cols-4">
+          <div className="flex-1 space-y-3 lg:col-span-2 py-3">
+            <SecundaryHeading className="text-center">Outros canais de contato:</SecundaryHeading>
+            <div className="text-black w-fit mx-auto grid gap-4 grid-cols-2 lg:grid-cols-4">
               <Link
                 target="_blank"
                 href="https://api.whatsapp.com/send?phone=5544997530950&text=Ol%C3%A1%20GalileoSoft,%20"
@@ -37,7 +41,7 @@ export default function Contato() {
                 <Card>
                   <div className="flex flex-col w-fit mx-auto">
                     <MdWhatsapp size={40} className="mx-auto" />
-                    <p className="text-base my-2 text-center">Whatsapp</p>
+                    <SecundaryText className="mt-3">Whatsapp</SecundaryText>
                   </div>
                 </Card>
               </Link>
@@ -49,7 +53,7 @@ export default function Contato() {
                 <Card>
                   <div className="flex flex-col w-fit mx-auto">
                     <MdPhone size={40} className="mx-auto" />
-                    <p className="my-2 text-center">Ligação</p>
+                    <SecundaryText className="mt-3">Ligação</SecundaryText>
                   </div>
                 </Card>
               </Link>
@@ -61,7 +65,7 @@ export default function Contato() {
                 <Card>
                   <div className="flex flex-col w-fit mx-auto">
                     <MdFacebook size={40} className="mx-auto" />
-                    <p className="my-2 text-center">Facebook</p>
+                    <SecundaryText className="mt-3">Facebook</SecundaryText>
                   </div>
                 </Card>
               </Link>
@@ -73,7 +77,7 @@ export default function Contato() {
                 <Card>
                   <div className="flex flex-col w-fit mx-auto">
                     <FaInstagram size={40} className="mx-auto" />
-                    <p className="my-2 text-center">Instagram</p>
+                    <SecundaryText className="mt-3">Instagram</SecundaryText>
                   </div>
                 </Card>
               </Link>
